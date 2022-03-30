2e53edd06498efd5895882b423eaf986ee514581
Suttie23 <76448183+Suttie23@users.noreply.github.com>
2022-03-30 12:02:24 +0100

    Pong Foundations
    Two paddles added and in position, ball added and in position.
    Movement implemented for left side paddle.

--- practical_1/main.cpp
+++ practical_1/main.cpp
@@ -1,21 +1,83 @@
 #include <SFML/Graphics.hpp>
 
-// Green Circle!
-int main() {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+using namespace sf;
+using namespace std;
 
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-        }
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-    return 0;
+const Keyboard::Key controls[4] = {
+	Keyboard::A,   // Player1 UP
+	Keyboard::Z,   // Player1 Down
+	Keyboard::Up,  // Player2 UP
+	Keyboard::Down // Player2 Down
+};
+const Vector2f paddleSize(25.f, 100.f);
+const float ballRadius = 10.f;
+const int gameWidth = 800;
+const int gameHeight = 600;
+const float paddleSpeed = 400.f;
+
+CircleShape ball;
+RectangleShape paddles[2];
+
+void Load() {
+	// Set size and origin of paddles
+	for (auto& p : paddles) {
+		p.setSize(paddleSize - Vector2f(3, 3));
+		p.setOrigin(paddleSize / 2.f);
+	}
+	// Set size and origin of ball
+	ball.setRadius(ballRadius);
+	ball.setOrigin(Vector2(ballRadius / 2, ballRadius / 2)); //Should be half the ball width and height
+	// reset paddle position
+	paddles[0].setPosition(Vector2(10.f + paddleSize.x / 2.f, gameHeight / 2.f));
+	paddles[1].setPosition(Vector2(765.f + paddleSize.x / 2.f, gameHeight / 2.f));
+	// reset Ball Position
+	ball.setPosition(Vector2f(gameWidth / 2, gameHeight /2));
+}
+
+void Update(RenderWindow& window) {
+	// Reset clock, recalculate deltatime
+	static Clock clock;
+	float dt = clock.restart().asSeconds();
+	// check and consume events
+	Event event;
+	while (window.pollEvent(event)) {
+		if (event.type == Event::Closed) {
+			window.close();
+			return;
+		}
+	}
+
+	// Quit Via ESC Key
+	if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+		window.close();
+	}
+
+	// handle paddle movement
+	float direction = 0.0f;
+	if (Keyboard::isKeyPressed(controls[0])) {
+		direction--;
+	}
+	if (Keyboard::isKeyPressed(controls[1])) {
+		direction++;
+	}
+	paddles[0].move(Vector2(0.f, direction * paddleSpeed * dt));
+}
+
+void Render(RenderWindow& window) {
+	// Draw Everything
+	window.draw(paddles[0]);
+	window.draw(paddles[1]);
+	window.draw(ball);
+}
+
+int main() {
+	RenderWindow window(VideoMode(gameWidth, gameHeight), "PONG");
+	Load();
+	while (window.isOpen()) {
+		window.clear();
+		Update(window);
+		Render(window);
+		window.display();
+	}
+	return 0;
 }
